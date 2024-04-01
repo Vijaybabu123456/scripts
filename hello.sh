e8e5195aa6a3e51e5dfa374e815992bd90d163c2
Vijaybabu123456 <vk433960@gmail.com>
2024-04-01 20:33:48 +0530

    updated the file

--- hello.sh
+++ hello.sh
@@ -6,3 +6,20 @@ read FNAME
 echo "What is your last name?"
 read LNAME
 echo "Hello, $FNAME $LNAME, welcome to my delops demo project".
+echo "what is the gender? M/F"
+read GEN
+echo "what is your age in years?"
+read Age
+if [[ $GEN == "F" ]]
+then
+    echo "Hello,Mr $FNAME $LNAME, welcome to my delops demo project".
+else
+    echo "Hello,Miss $FNAME $LNAME, welcome to my delops demo project".
+fi
+
+if [[ $Age -lt 21 ]]
+then
+    echo "consider as minor"
+else
+    echo "consider as major"
+fi
